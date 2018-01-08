0954b4c20d90b9e5e1fe82691b1b2ebf3a81bc37
Demosth3nes <jermaine911@hotmail.com>
2018-01-08 15:28:38 +0000

    adding test and missing packages

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import styles from './css/index.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import styles from "./css/index.css";
 
 export default class Hello extends Component {
   render() {
-    return (
-      <div>
-        Hello from reacts
-      </div>
-    );
+    return <div>Hello from reacts test</div>;
   }
 }
